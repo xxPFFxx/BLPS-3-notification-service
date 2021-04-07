198eec238bd933374ac3ccbeb3d7b00017fd2946
Daniil Demichev <daniil_demichev@mail.ru>
2021-04-07 09:55:24 +0300

    Test

--- complete/src/main/java/com/example/uploadingfiles/config/StorageConfig.java
+++ complete/src/main/java/com/example/uploadingfiles/config/StorageConfig.java
@@ -16,10 +16,10 @@ public class StorageConfig {
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
 //        dataSourceBuilder.url("jdbc:postgresql://localhost:5431/studs");
 //        dataSourceBuilder.username("s265096");
-//        dataSourceBuilder.password("***");
+//        dataSourceBuilder.password("vrm232");
         dataSourceBuilder.url("jdbc:postgresql://localhost:5432/postgres");
         dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("***");
+        dataSourceBuilder.password("Postgreshehmda");
 
         return dataSourceBuilder.build();
     }
